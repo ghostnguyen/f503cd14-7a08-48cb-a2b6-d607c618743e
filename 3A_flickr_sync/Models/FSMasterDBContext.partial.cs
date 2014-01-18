12480ca27fb4acbf93c43478daacff096b041a05
ghostnguyen <ghostnguyen@gmail.com>
2014-01-18 16:21:21 +0700

    a

--- 3A_flickr_sync/Models/FSMasterDBContext.partial.cs
+++ 3A_flickr_sync/Models/FSMasterDBContext.partial.cs
@@ -18,11 +18,26 @@ namespace _3A_flickr_sync.Models
 
         private static string GetConnectionString()
         {
+            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string userFilePath = Path.Combine(localAppData, "3A-Software");
+
+            if (!Directory.Exists(userFilePath))
+                Directory.CreateDirectory(userFilePath);
+
             SqlConnectionStringBuilder connStrBuilder = new SqlConnectionStringBuilder(AppSetting.FSMasterDBConnectionStr);
 
             FileInfo fileInfo = new FileInfo(connStrBuilder.AttachDBFilename);
 
-            connStrBuilder.AttachDBFilename = AppDomain.CurrentDomain.BaseDirectory + fileInfo.Name;
+            //connStrBuilder.AttachDBFilename = AppDomain.CurrentDomain.BaseDirectory + fileInfo.Name;
+            connStrBuilder.AttachDBFilename = Path.Combine(userFilePath, fileInfo.Name);
+
+            ////if it's not already there, 
+            ////copy the file from the deployment location to the folder
+            //string sourceFilePath = Path.Combine(
+            //  System.Windows.Forms.Application.StartupPath, "MyFile.txt");
+            //string destFilePath = Path.Combine(userFilePath, "MyFile.txt");
+            //if (!File.Exists(destFilePath))
+            //    File.Copy(sourceFilePath, destFilePath);
 
             return connStrBuilder.ConnectionString;
         }
